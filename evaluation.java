152ad8f45eaf305589ccabc2a10dff2128e3c797
Munaze <mohitkumar97611khola@gmail.com>
2019-09-26 16:06:55 +0530

    mohit

--- evaluation.java
+++ evaluation.java
@@ -3,7 +3,16 @@ import java.util.scanner
 
 public class menu{ 
 
-public int add()
+public int add(int num1,int num2){
+int res = num1 - num2
+return res;
+}
+
+public int multiply(int num1,int num2){
+
+int res = num1 - num2
+return res;
+}
 public static void main(String []args){
 
 }
