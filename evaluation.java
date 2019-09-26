2ebf2995552a892bf99c66a77e4c34376e13e6b3
shikari666 <vemulapallirishika98@gmail.com>
2019-09-26 16:10:39 +0530

    rishika

--- evaluation.java
+++ evaluation.java
@@ -8,9 +8,14 @@ int res = num1 - num2
 return res;
 }
 
-public int multiply(int num1,int num2){
+public int subtract(int num1,int num2){
 
 int res = num1 - num2
+return res;
+}
+public int multiply(int num1,int num2){
+
+int res = num1 *num2
 return res;
 }
 public static void main(String []args){
